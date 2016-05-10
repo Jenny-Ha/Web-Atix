0977a70fbba34dd046edd1a55ef977f890e4f656
Jenny <jennyha069@gmail.com>
2016-05-09 19:38:49 -0500

    Added photos

--- js/main.js
+++ js/main.js
@@ -24,4 +24,18 @@ window.addEventListener('scroll', function (e) {
     } else {
         menu.classList.remove('navbar-fixed');
     }
+    
+    /* On scroll actives items menu 
+    var scrollPos = $(document).scrollTop() + 50;
+    $('.top-menu li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.top-menu li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });    */
 });
