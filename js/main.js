d21183075d2d4319998afcb536ba3bc389271938
Jenny <jennyha069@gmail.com>
2016-05-12 17:19:04 -0500

    Change styles f

--- js/main.js
+++ js/main.js
@@ -30,19 +30,7 @@ window.addEventListener('scroll', function (e) {
         menu.classList.remove('navbar-fixed');
     }
     
-    /* On scroll actives items menu 
-    var scrollPos = $(document).scrollTop() + 50;
-    $('.top-menu li a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.top-menu li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });    */
+    
 });
 
 /* Funcion para mostrar menu en dispositivos pequeñños */
