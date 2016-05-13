726c6e3dfc7d27c8a4287d669d55f70a6c2d02b3
Jenny <jennyha069@gmail.com>
2016-05-12 19:57:14 -0500

    Fixed navbar fixed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(window).load(function() {
     $('body').addClass('body-visible').fadeIn('slow');
     $('.loader').fadeOut('slow');
     
-    
     /* Personalizar los elementos de Tipeo */
     $(function(){
         $(".type-it").typed({
@@ -20,9 +19,9 @@ $(window).load(function() {
 /* Para el menu scroll */
 var menu = document.getElementById('wrap_menu');
 var section = document.getElementById('nosotros');
-var alturaNav = menu.clientHeight;
-var altura = section.offsetTop - 71;
-//console.log(altura);
+var items_Menu = $('nav li a');
+var alturaNav = 70; //menu.clientHeight;
+var altura = section.offsetTop - (alturaNav + 1);
 
 window.addEventListener('scroll', function (e) {
    if (window.pageYOffset > altura) {
@@ -30,26 +29,37 @@ window.addEventListener('scroll', function (e) {
     } else {
         menu.classList.remove('navbar-fixed');
     }
+    
+    /* On scroll actives items menu */
+    var scrollPos = $(document).scrollTop() + alturaNav;
+   items_Menu.each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.top-menu li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });    
 });
+
 
 /* Smooth scrolling para anclas*/
  $('a.smooth').click(function(e){
     e.preventDefault();
     $('html, body').stop().animate({
-        scrollTop: $($(this).attr('href')).offset().top - 70}
+        scrollTop: $($(this).attr('href')).offset().top - (alturaNav - 1)}
         , 500);
 });
 
-/*
-    $('a.smooth').live('click', function(e) {  
-        e.preventDefault();
-        var link = $(this);  
-        var anchor  = link.attr('href');  
-        $('html, body').stop().animate({  
-            scrollTop: $(anchor).offset().top - ($('#nav_wrapper').height()+21)
-        }, 1500, 'easeOutBack');  
-    });  
-    */
+/* Active items-menu  */
+items_Menu.click( function (e) {
+    items_Menu.removeClass('active');
+    $(this).addClass('active');   
+})
+
    
 /* Funcion para mostrar menu en dispositivos pequeñños */
 $(".button-collapse").sideNav();
@@ -58,5 +68,5 @@ $(".button-collapse").sideNav();
 $('.scrollspy').scrollSpy();
 
 /* Modal */
-    $('.modal-trigger').leanModal();
+ $('.modal-trigger').leanModal();
 
